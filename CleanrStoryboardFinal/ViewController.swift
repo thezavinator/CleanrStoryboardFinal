22f50c2293cae8275f7d2935be204821b15b564e
thezavinator <thezave@gmail.com>
2025-01-05 11:44:28 -0600

    Initial Commit

---
+++ CleanrStoryboardFinal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanrStoryboardFinal
+//
+//  Created by Zavier Hofstetter on 1/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CleanrStoryboardFinalTests/CleanrStoryboardFinalTests.swift
@@ -0,0 +1,17 @@
+//
+//  CleanrStoryboardFinalTests.swift
+//  CleanrStoryboardFinalTests
+//
+//  Created by Zavier Hofstetter on 1/5/25.
+//
+
+import Testing
+@testable import CleanrStoryboardFinal
+
+struct CleanrStoryboardFinalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
